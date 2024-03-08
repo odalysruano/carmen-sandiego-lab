d80822dd2faa26782be6d215915fd1ab79891460
odalysruano <odalys.r.ruano@gmail.com>
2024-03-08 16:33:37 -0500

    solve clue 6

--- clues.sql
+++ clues.sql
@@ -69,7 +69,14 @@ LIKE 'Serra%';
 -- follow right behind you!
 
 -- Write SQL query here
+SELECT c.capital, i.id, i.name
+FROM public.country c 
+JOIN public.city i
+ON c.capital = i.id
+WHERE c.code = 'BRA';
 
+-- name: Brasilia
+-- capital: 211
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
 -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
