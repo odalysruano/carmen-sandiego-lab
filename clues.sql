a558cb05ddc4af312090cd64d2a2e6dbd36daa05
odalysruano <odalys.r.ruano@gmail.com>
2024-03-08 16:48:13 -0500

    solve clue 7

--- clues.sql
+++ clues.sql
@@ -94,3 +94,8 @@ WHERE c.code = 'BRA';
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+SELECT i.name
+FROM public.city i
+WHERE i.population = '91084';
+
+--name: Santa Monica
